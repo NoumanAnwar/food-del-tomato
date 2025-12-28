b38278b80a4de2e051238962bf352ab3982733fd
NoumanAnwar <maan.k_55@yahoo.com>
2025-12-28 13:02:41 +0500

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className='footer-content'> 
+       <div className='footer-content-left'>
+        
+        <img src={assets.logo} alt=''/>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, minima quia. Magnam, hic eum unde repellat officiis earum rerum consectetur qui saepe quisquam, eveniet corrupti cumque placeat. Ratione, molestias corrupti?</p>
+        <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt='facebook' />            
+            <img src={assets.twitter_icon} alt='twitter' />
+            <img src={assets.linkedin_icon} alt='linkedin' />
+        </div>
+        </div>
+        <div className="footer-content-center">
+            <h2>COMPANY</h2>
+            <ul>
+
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+        <div className="footer-content-right">
+            <h2>GET IN TOUCH</h2>
+            <ul>
+                <li>+1-212-456-7890</li>
+                <li>contact@tomato.com</li>
+            </ul>
+
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2025 @ NYXTHERON - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
